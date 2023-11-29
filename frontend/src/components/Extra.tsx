c2e46e3e5df126a260b5c63d656910462da23448
Kevin <kevinglennon01@gmail.com>
2023-11-29 20:02:22 +0000

    Removed errors from Extra

--- frontend/src/components/Extra.tsx
+++ frontend/src/components/Extra.tsx
@@ -1,23 +1,37 @@
+import React from "react";
 import Styles from "../styles/card.module.css";
 
-const Extra = (props: any) => {
+interface Country {
+  startOfweek: string;
+  fifa: string;
+  languages: Record<string, string>;
+  // Define other properties if available
+}
+
+interface ExtraProps {
+  data: Country[];
+}
+
+const Extra: React.FC<ExtraProps> = (props) => {
   const { data } = props;
 
   return (
     <>
       <div className={Styles.innerContainer}>
         <div className={Styles.cardTitle}>Extra</div>
-        {data.map((country: any, index: number) => (
+        {data.map((country: Country, index: number) => (
           <div key={index}>
-            <div>Start of week: {country.startOfweek}</div>
-            <div>FIFA: {country.fifa}</div>
+            <div>Start of week: {country.startOfweek || "N/A"}</div>
+            <div>FIFA: {country.fifa || "N/A"}</div>
             <div>
               Languages:
-              {Object.entries(country.languages).map(([code, languageName]) => (
-                <span key={code}>
-                  <div>{languageName}</div>
-                </span>
-              ))}
+              {Object.entries(country.languages || {}).map(
+                ([code, languageName]) => (
+                  <span key={code}>
+                    <div>{languageName}</div>
+                  </span>
+                )
+              )}
             </div>
           </div>
         ))}
