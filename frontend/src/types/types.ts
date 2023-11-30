03adda2a83b80744f12182045ade0072967fbd48
Kevin <kevinglennon01@gmail.com>
2023-11-30 00:11:08 +0000

    Fixing imports and types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,27 +1,21 @@
-// types.ts
-
-// types.ts
-
 export interface Country {
   idd: {
     suffixes: string;
-    // Other properties related to idd if available
   };
   name: {
     common: string;
-    // Other properties related to name if available
   };
   flags: {
     png: string;
-    // Other properties related to flags if available
   };
-  startOfweek: string; // Example property
-  fifa: string; // Example property
-  languages: Record<string, string>; // Example property
+  startOfweek: string;
+  fifa: string;
+  languages: Record<string, string>;
   capital: string;
   continents: string;
   latlng: number[];
   borders: string[];
+  id: number;
 }
 
 export interface CountryInfoProps {
