6c812b1ff2b9a0365f3b3f1f2ae08fa7a439b192
Kevin <kevinglennon01@gmail.com>
2023-11-30 16:30:00 +0000

    Changed backend api and added axios

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const https = require("https");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
 
-app.get("/api/data/:country", (req, res) => {
-  const country = req.params.country;
-  https
-    .get(
-      `https://restcountries.com/v3.1/name/${country}?fullText=true`,
-      (response) => {
-        let data = "";
+app.get("/api/data/:country", async (req, res) => {
+  try {
+    const country = req.params.country;
+    const externalApiUrl = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
 
-        response.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        response.on("end", () => {
-          try {
-            const jsonData = JSON.parse(data);
-            res.json(jsonData);
-          } catch (error) {
-            res
-              .status(500)
-              .json({ error: "Error parsing data from external API" });
-          }
-        });
-      }
-    )
-    .on("error", (error) => {
-      res.status(500).json({ error: "Error fetching data from external API" });
-    });
+    const response = await axios.get(externalApiUrl);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from external API:", error);
+    res.status(500).json({ error: "Error fetching data from external API" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
